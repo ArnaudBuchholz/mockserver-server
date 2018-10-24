e3e9d3927f3c3c906ad561631847e209f90d0023
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-24 16:33:06 -0400

    Loading works !

--- index.js
+++ index.js
@@ -6,31 +6,30 @@ const { JSDOM } = jsdom
 const openui5BaseDir = path.join(__dirname, 'bower_components/openui5-sap.ui.core/resources')
 
 const read = (type, url) => {
-    const match = /(?:\/|^)resources\/(.*)$/.exec(url)
-    if (match) {
-        let content
-        if (url.endsWith('css')) {
-            content = ''
-        } else {
-            content = fs.readFileSync(path.join(openui5BaseDir, match[1])).toString()
-        }
-        console.log(type.magenta, 'GET'.cyan, url.cyan, '200'.green, content.length)
-        return content
+  const match = /(?:\/|^)resources\/(.*)$/.exec(url)
+  if (match) {
+    let content
+    if (url.endsWith('css')) {
+      content = '/* no style */' // Must not be empty
+    } else {
+      content = fs.readFileSync(path.join(openui5BaseDir, match[1])).toString()
     }
+    console.log(type.magenta, 'GET'.cyan, url.cyan, '200'.green, content.length)
+    return content
+  }
 }
 
 class ResourceLoader extends jsdom.ResourceLoader {
   fetch (url, options) {
     const content = read('RES', url)
     if (content) {
-        return Promise.resolve(content)
+      return Promise.resolve(content)
     }
     return super.fetch(url, options)
   }
 }
 
-console.log('Creating a simulated browser...'.white)
-
+// Creating a simulated browser
 const browser = new JSDOM(fs.readFileSync('./index.html').toString(), {
   url: 'https://mockserver.server.net/',
   referrer: 'https://mockserver.server.net/',
@@ -40,7 +39,7 @@ const browser = new JSDOM(fs.readFileSync('./index.html').toString(), {
   runScripts: 'dangerously',
   resources: new ResourceLoader(),
   beforeParse: window => {
-    console.log('Merging window context with global one...'.white)
+    // Merging window context with global one
     global.window = window
     'document,XMLHttpRequest'
       .split(',')
@@ -49,21 +48,21 @@ const browser = new JSDOM(fs.readFileSync('./index.html').toString(), {
       })
     // Compatibility layer (see https://developer.mozilla.org/en-US/docs/Web/API/PerformanceTiming/fetchStart)
     window.performance.timing = {
-        fetchStart: 0
+      navigationStart: new Date().getTime(),
+      fetchStart: new Date().getTime()
     }
-    console.log('Mocking the XHR object...'.white)
+    // Mocking the XHR object to allow synchronous loading of OpenUI5 files
     const fakeXhr = require('nise').fakeXhr
     const xhrHandler = fakeXhr.useFakeXMLHttpRequest()
     xhrHandler.onCreate = xhr => {
-      const _send = xhr.send
       xhr.send = data => {
-          try {
-              xhr.respond(200, {}, read('XHR', xhr.url))
-              return
-          } catch (err) {
-              console.log('XHR'.magenta, 'GET'.cyan, xhr.url.cyan, '404'.red, err.code)
-          }
-          xhr.respond(404)
+        try {
+          xhr.respond(200, {}, read('XHR', xhr.url))
+          return
+        } catch (err) {
+          console.log('XHR'.magenta, 'GET'.cyan, xhr.url.cyan, '404'.red, err.code)
+        }
+        xhr.respond(404)
       }
     }
     window.XMLHttpRequest = global.XMLHttpRequest
@@ -72,4 +71,3 @@ const browser = new JSDOM(fs.readFileSync('./index.html').toString(), {
 
 const window = browser.window
 console.log(window.location.toString())
-console.log('end.')
