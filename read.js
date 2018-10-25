188f97a87b82033e00d86bb837c5115a3d9c50ab
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-25 18:01:04 -0400

    Complete read with traces

---
+++ read.js
@@ -0,0 +1,31 @@
+require('colors')
+const fs = require('fs')
+const path = require('path')
+const openui5BaseDir = path.join(__dirname, 'bower_components/openui5-sap.ui.core/resources')
+
+module.exports = (type, url) => {
+  try {
+    const match = /(?:\/|^)resources\/(.*)$/.exec(url) // Expect local resources to be under resources
+    if (!match) {
+      throw new Error('not a resource')
+    }
+    if (url.endsWith('css')) {
+      return '/* style must not be empty */'
+    }
+    let resourcePath
+    if (match[1].startsWith('mock/')) {
+      if (match[1].endsWith('xml')) {
+        debugger // TODO remove
+      }
+      resourcePath = path.join(__dirname, match[1])
+    } else {
+      resourcePath = path.join(openui5BaseDir, match[1])
+    }
+    fs.accessSync(resourcePath, fs.constants.R_OK)
+    const content = fs.readFileSync(resourcePath).toString()
+    console.log(type.magenta, 'GET'.cyan, url.cyan, '200'.green, content.length)
+    return content
+  } catch (e) {}
+  console.log(type.magenta, 'GET'.cyan, url.cyan, '404'.red)
+  return null
+}
