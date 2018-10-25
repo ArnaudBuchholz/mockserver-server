456aa0e0197e6dda0b7a45f3459876df9fd38e21
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-25 18:00:55 -0400

    Modularizes

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-require('colors')
-const fs = require('fs')
-const path = require('path')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
-const openui5BaseDir = path.join(__dirname, 'bower_components/openui5-sap.ui.core/resources')
-
-const read = (type, url) => {
-  const match = /(?:\/|^)resources\/(.*)$/.exec(url)
-  if (match) {
-    let content
-    if (url.endsWith('css')) {
-      content = '/* no style */' // Must not be empty
-    } else if (match[1].startsWith('mock/')) {
-      content = fs.readFileSync(path.join(__dirname, match[1])).toString()
-    } else {
-      content = fs.readFileSync(path.join(openui5BaseDir, match[1])).toString()
-    }
-    console.log(type.magenta, 'GET'.cyan, url.cyan, '200'.green, content.length)
-    return content
-  }
-  debugger
-}
+const fs = require('fs')
+const read = require('read')
 
 class ResourceLoader extends jsdom.ResourceLoader {
   fetch (url, options) {
@@ -28,7 +9,7 @@ class ResourceLoader extends jsdom.ResourceLoader {
     if (content) {
       return Promise.resolve(content)
     }
-    return super.fetch(url, options)
+    return null
   }
 }
 
@@ -54,21 +35,6 @@ const browser = new JSDOM(fs.readFileSync('./index.html').toString(), {
       navigationStart: new Date().getTime(),
       fetchStart: new Date().getTime()
     }
-    // Mocking the XHR object to allow synchronous loading of OpenUI5 files
-    const fakeXhr = require('nise').fakeXhr
-    const xhrHandler = fakeXhr.useFakeXMLHttpRequest()
-    xhrHandler.onCreate = xhr => {
-      xhr.send = data => {
-        try {
-          xhr.respond(200, {}, read('XHR', xhr.url))
-          return
-        } catch (err) {
-          console.log('XHR'.magenta, 'GET'.cyan, xhr.url.cyan, '404'.red, err.code)
-        }
-        xhr.respond(404)
-      }
-    }
-    window.XMLHttpRequest = global.XMLHttpRequest
   }
 })
 
